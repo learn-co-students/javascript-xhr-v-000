a6952eaa276a1f364fd59e2c9903b07ec552a31a
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-04-03 00:33:29 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function showRepositories(event, data) {
   repoList += "</ul>"
   document.getElementById("repositories").innerHTML = repoList
 }
+
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
