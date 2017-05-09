96d2fc2ecea837ddf74d5c231932680883a63e2e
Keegan Leitz <kjleitz@gmail.com>
2017-05-09 16:47:40 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function showRepositories(event, data) {
+  console.log(this.responseText);
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
