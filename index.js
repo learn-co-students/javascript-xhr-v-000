b107e6f78b573dbd4d849274d7265afc8619fabc
Jrodr4544 <maskdbandit@gmail.com>
2018-09-08 17:31:15 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@
 
     function getRepositories() {
       const req = new XMLHttpRequest();
+      req.addEventListener("load", showRepositories);
       req.open("GET", "https://api.github.com/users/octocat/repos");
       req.send();
     }
